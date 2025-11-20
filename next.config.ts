5cf1b902e2507f62be1074ac9a67ae81469d14b5
Juan Ignacio Vergara <ignacio.vergara282@gmail.com>
2025-11-20 07:32:39 -0500

    changed eslint configs for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Allow production builds to succeed even if there are
+    // ESLint warnings/errors. This is helpful for rapid demos.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
